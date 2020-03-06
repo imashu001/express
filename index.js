67648a830afddbf2829bf6390eb312334386212d
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-03-06 19:10:07 +0530

    another controller created view engine set up

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 1703;
 
 app.use('/',require('./routes/index'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in loading server : ${err}`);
