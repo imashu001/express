6849a044548e8f8a54e1d161856cda41a03fbca4
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-03-26 02:06:54 +0530

    Authorization via jwt token created

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,5 +1,8 @@
 const Post = require('../../../models/post');
 const Comment = require('../../../models/comment');
+
+
+
 module.exports.index = async function(req, res){
 
 
@@ -25,7 +28,7 @@ module.exports.destroy = async function(req, res){
     try{
         let post = await Post.findById(req.params.id);
 
-        // if (post.user == req.user.id){
+        if (post.user == req.user.id){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
@@ -35,10 +38,11 @@ module.exports.destroy = async function(req, res){
             return res.json(200, {
                 message: "Post and associated comments deleted successfully!"
             });
-        // }else{
-        //     req.flash('error', 'You cannot delete this post!');
-        //     return res.redirect('back');
-        // }
+        }else{
+           return res.json(401,{
+               message:"You cannot delete this Post"
+           })
+        }
 
     }catch(err){
         console.log('********', err);
