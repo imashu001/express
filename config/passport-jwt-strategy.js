a9d0f85c494cced8afd175ba55b496b6ba5d9559
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-03-28 07:15:23 +0530

    Like option added

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,14 +1,16 @@
 const passport = require('passport');
 const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJWT  = require('passport-jwt').ExtractJwt;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
 
 const User = require('../models/user');
 
-let opts = {
-    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey : 'codeial'
 
+let opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'codeial'
 }
+
+
 passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
 
     User.findById(jwtPayLoad._id, function(err, user){
--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+
 const multer = require('multer');
 const path = require('path');
-const AVATAR_PATH = path.join("/uploads/users/avatar");
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -23,19 +24,23 @@ const userSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now());
     }
-  })
+  });
 
-  //STATIC METHOD 
-  userSchema.statics.uploadedAvatar = multer({storage:storage}).single('avatar');
-  userSchema.statics.avatarPath = AVATAR_PATH;
-  
+
+// static
+userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
+
+
 
 const User = mongoose.model('User', userSchema);
 
